aaa7d562d4346690442229d765da8a7a85ce50da
hưng <nphbo1998@gmail.com>
2020-01-15 22:21:35 +0700

    Updates

---
+++ precache-manifest.bff82a7f30c90dfd8cd79c871b39d04b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ae65c96d7fd5f75316c04a0cb74af31",
+    "url": "/Monster-rolodex/index.html"
+  },
+  {
+    "revision": "f5ab77f65ccf0872134b",
+    "url": "/Monster-rolodex/static/css/main.355fe139.chunk.css"
+  },
+  {
+    "revision": "c728ba4a2366e885ec54",
+    "url": "/Monster-rolodex/static/js/2.d8d20882.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Monster-rolodex/static/js/2.d8d20882.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f5ab77f65ccf0872134b",
+    "url": "/Monster-rolodex/static/js/main.5fe6b904.chunk.js"
+  },
+  {
+    "revision": "f7437328683153f361a5",
+    "url": "/Monster-rolodex/static/js/runtime-main.b7357705.js"
+  }
+]);
